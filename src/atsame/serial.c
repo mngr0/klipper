90426cf445fa0515d623c7a052c4b259a01e6944
mngr0 <marco.negrini6@studio.unibo.it>
2021-02-11 19:26:48 +0100

    moved serial to pin header. testing with logic analyzer. reduced speed to 115200

--- src/atsame/serial.c
+++ src/atsame/serial.c
@@ -13,37 +13,37 @@
 void
 serial_enable_tx_irq(void)
 {
-    SERCOM2->USART.INTENSET.reg = SERCOM_USART_INTENSET_DRE;
+    SERCOM0->USART.INTENSET.reg = SERCOM_USART_INTENSET_DRE;
 }
 
 void
-SERCOM2_Handler(void)
+SERCOM0_Handler(void)
 {
-    uint32_t status = SERCOM2->USART.INTFLAG.reg;
+    uint32_t status = SERCOM0->USART.INTFLAG.reg;
     if (status & SERCOM_USART_INTFLAG_RXC)
-        serial_rx_byte(SERCOM2->USART.DATA.reg);
+        serial_rx_byte(SERCOM0->USART.DATA.reg);
     if (status & SERCOM_USART_INTFLAG_DRE) {
         uint8_t data;
         int ret = serial_get_tx_byte(&data);
         if (ret)
-            SERCOM2->USART.INTENCLR.reg = SERCOM_USART_INTENSET_DRE;
+            SERCOM0->USART.INTENCLR.reg = SERCOM_USART_INTENSET_DRE;
         else
-            SERCOM2->USART.DATA.reg = data;
+            SERCOM0->USART.DATA.reg = data;
     }
 }
 
-DECL_CONSTANT_STR("RESERVE_PINS_serial", "PB24,PB25");
+DECL_CONSTANT_STR("RESERVE_PINS_serial", "PA05,PA04");
 
 void
 serial_init(void)
 {
     // Enable serial clock
-    enable_pclock(SERCOM2_GCLK_ID_CORE, ID_SERCOM2);
+    enable_pclock(SERCOM0_GCLK_ID_CORE, ID_SERCOM0);
     // Enable pins
-    gpio_peripheral(GPIO('B', 24), 'D', 0);
-    gpio_peripheral(GPIO('B', 25), 'D', 0);
+    gpio_peripheral(GPIO('A', 4), 'D', 0);
+    gpio_peripheral(GPIO('A', 5), 'D', 0);
     // Configure serial
-    SercomUsart *su = &SERCOM2->USART;
+    SercomUsart *su = &SERCOM0->USART;
     su->CTRLA.reg = 0;
     uint32_t areg = (SERCOM_USART_CTRLA_MODE(1)
                      | SERCOM_USART_CTRLA_DORD
@@ -52,7 +52,7 @@ serial_init(void)
                      | SERCOM_USART_CTRLA_TXPO(1));
     su->CTRLA.reg = areg;
     su->CTRLB.reg = SERCOM_USART_CTRLB_RXEN | SERCOM_USART_CTRLB_TXEN;
-    uint32_t freq = get_pclock_frequency(SERCOM2_GCLK_ID_CORE);
+    uint32_t freq = get_pclock_frequency(SERCOM0_GCLK_ID_CORE);
     uint32_t baud8 = freq / (2 * CONFIG_SERIAL_BAUD);
     su->BAUD.reg = (SERCOM_USART_BAUD_FRAC_BAUD(baud8 / 8)
                     | SERCOM_USART_BAUD_FRAC_FP(baud8 % 8));
@@ -60,10 +60,10 @@ serial_init(void)
     su->INTENSET.reg = SERCOM_USART_INTENSET_RXC;
     su->CTRLA.reg = areg | SERCOM_USART_CTRLA_ENABLE;
 // #if CONFIG_MACH_SAME54
-    armcm_enable_irq(SERCOM2_Handler, SERCOM2_0_IRQn, 0);
-    armcm_enable_irq(SERCOM2_Handler, SERCOM2_1_IRQn, 0);
-    armcm_enable_irq(SERCOM2_Handler, SERCOM2_2_IRQn, 0);
-    armcm_enable_irq(SERCOM2_Handler, SERCOM2_3_IRQn, 0);
+    armcm_enable_irq(SERCOM0_Handler, SERCOM0_0_IRQn, 0);
+    armcm_enable_irq(SERCOM0_Handler, SERCOM0_1_IRQn, 0);
+    armcm_enable_irq(SERCOM0_Handler, SERCOM0_2_IRQn, 0);
+    armcm_enable_irq(SERCOM0_Handler, SERCOM0_3_IRQn, 0);
 // #endif
 }
 DECL_INIT(serial_init);
