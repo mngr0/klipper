5795e52d117fd78e5999955b0cd531bad51e426d
mngr0 <marco.negrini6@studio.unibo.it>
2021-02-15 12:02:11 +0100

    fixed PAD pinout

--- src/atsame/serial.c
+++ src/atsame/serial.c
@@ -48,8 +48,8 @@ serial_init(void)
     uint32_t areg = (SERCOM_USART_CTRLA_MODE(1)
                      | SERCOM_USART_CTRLA_DORD
                      | SERCOM_USART_CTRLA_SAMPR(1)
-                     | SERCOM_USART_CTRLA_RXPO(3)
-                     | SERCOM_USART_CTRLA_TXPO(1));
+                     | SERCOM_USART_CTRLA_RXPO(1)
+                     | SERCOM_USART_CTRLA_TXPO(0));
     su->CTRLA.reg = areg;
     su->CTRLB.reg = SERCOM_USART_CTRLB_RXEN | SERCOM_USART_CTRLB_TXEN;
     uint32_t freq = get_pclock_frequency(SERCOM2_GCLK_ID_CORE);
